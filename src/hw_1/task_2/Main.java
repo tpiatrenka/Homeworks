b7a4261c54b50b0b0ae3c90a73fe221d639c7c94
Tatiana Piatrenka <tpiatrenka@gmail.com>
2022-05-29 23:11:09 +0200

    Move homework 1

--- src/hw_1/task_2/Main.java
+++ src/hw_1/task_2/Main.java
@@ -1,5 +1,8 @@
+package hw_1.task_2;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Tatiana!");
     }
+
 }
