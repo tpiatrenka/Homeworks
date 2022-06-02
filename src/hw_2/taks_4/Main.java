3139c7e4aa506140ce602625398a7e9104394bd4
Mary Stankevich <marrycitty@mail.ru>
2022-06-02 11:42:30 +0200

    Update Main.java

--- src/hw_2/taks_4/Main.java
+++ src/hw_2/taks_4/Main.java
@@ -1,11 +1,13 @@
 package hw_2.taks_4;
 
+//+
 public class Main {
     //Составьте программу, вычисляющую A*B, не пользуясь операцией умножения.
     public static void main(String[] args) {
         int a = 4;
         int b = 5;
         int multi = 0;
+        //Количество повтора цикла может быть оптимизировано, но это опционально 
         for (int i = 0; i < a; i++) {
             multi += b;
         }
