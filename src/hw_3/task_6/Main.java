421c4cbb0b3e30234766a280a08fd91ac5c562c5
Mary Stankevich <marrycitty@mail.ru>
2022-06-08 21:01:34 +0200

    Update Main.java

--- src/hw_3/task_6/Main.java
+++ src/hw_3/task_6/Main.java
@@ -1,5 +1,6 @@
 package hw_3.task_6;
 
+//+
 public class Main {
     //Есть массив чисел. Необходимо создать другой массив содержащий только четные элементы из первого
     public static void main(String[] args) {
@@ -7,7 +8,7 @@ public class Main {
         double[] evenArray = new double[(myArray.length + 1)/2];
         for (int i = 0; i < myArray.length; i+=2) {
             evenArray[i/2] = myArray[i];
-            System.out.println(evenArray[i/2]);
+            System.out.println(evenArray[i/2]); //можно в конце вывести весь получившийся массив, вне цикла
         }
     }
 
