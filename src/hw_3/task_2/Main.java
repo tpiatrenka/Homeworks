2aa86efa53e4e862daef645f71788dc358d43a5a
Mary Stankevich <marrycitty@mail.ru>
2022-06-08 20:35:49 +0200

    Update Main.java

--- src/hw_3/task_2/Main.java
+++ src/hw_3/task_2/Main.java
@@ -1,12 +1,13 @@
 package hw_3.task_2;
 
+//+ 
 public class Main {
     //Есть массив чисел. Найти среднее арифметическое число элементов массива
 
     public static void main(String[] args) {
         double[] myArray = new double[]{1,2,3,4,5,6,7,8,9};
         double sum = 0;
-        double mean = 0;
+        double mean = 0; //чаще для среднего арихметического употребляется слово average
         for (int i = 0; i < myArray.length; i++) {
             sum += myArray[i];
         }
