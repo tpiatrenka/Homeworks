ea0c2ebb90054b7172dccc8feac2ad67d12096d8
Mary Stankevich <marrycitty@mail.ru>
2022-06-13 13:48:36 +0200

    Update MacBook.java

--- src/hw_5/task_1/MacBook.java
+++ src/hw_5/task_1/MacBook.java
@@ -1,6 +1,7 @@
 package hw_5.task_1;
 //Есть интерфейс Computer,у которого есть методы включить, выключить и резет. Создайте класс, который бы имплементировал
 //этот интерфейс
+//+
 public class MacBook implements Computer{
     @Override
     public void turnOn() {
