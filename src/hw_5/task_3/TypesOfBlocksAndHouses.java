db8157f975f64735beb0f87874ad8ad2b3a366f2
Mary Stankevich <marrycitty@mail.ru>
2022-06-14 21:53:00 +0200

    Update TypesOfBlocksAndHouses.java

--- src/hw_5/task_3/TypesOfBlocksAndHouses.java
+++ src/hw_5/task_3/TypesOfBlocksAndHouses.java
@@ -1,7 +1,8 @@
 package hw_5.task_3;
 
+//+- Интерейс может называться просто House
 public interface TypesOfBlocksAndHouses {
-    int getTheFloor();
-    boolean turnOnTheHeating();
+    int getTheFloor(); //обычно в названиях методов артикли опускаются -> getFloor, но метод должен возвращать количество этажей, пэтому еще лучше будет getFloorsNumber 
+    boolean turnOnTheHeating(); 
     int getTheNumberOfResidents();
 }
