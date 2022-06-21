c3dffbfc1b14abc1b20e95e5975bee2f9b937b77
Mary Stankevich <marrycitty@mail.ru>
2022-06-21 21:36:02 +0200

    Update Main.java

--- src/hw_7/task_5/Main.java
+++ src/hw_7/task_5/Main.java
@@ -2,6 +2,7 @@ package hw_7.task_5;
 
 import java.util.Arrays;
 
+//+
 //Есть массив слов. Нужно отфильтровать его так, чтобы в выходном массиве оказались слова, которые начинаются с буквы “W”/”w”.
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class Main {
         String filtered = "";
         for (int i = 0; i < string.length; i++) {
             if (string[i].toLowerCase().startsWith("w")) {
-                filtered = filtered.concat(string[i] + ",");
+                filtered = filtered.concat(string[i] + ","); //хитрый ход :) но когда речь идет о нескольких конкатенациях, лучше использовать билдер или баффер
             }
         }
         String[] filteredArray = filtered.split(",");
