42874e0b88bbfa2057b21eba52490ef7ba9a6aa6
Tatiana Piatrenka <tpiatrenka@gmail.com>
2022-06-27 17:49:02 +0200

    Add Homework 9

---
+++ src/hw_9/task_1/Main.java
@@ -0,0 +1,22 @@
+package hw_9.task_1;
+//Есть массив чисел, заполнить его можете любыми цифрами. Нужно записать его в файл
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        try {
+            FileOutputStream fos = new FileOutputStream("src/hw_9/task_1/numbers.txt");
+            for (int i = 0; i < numbers.length; i++)
+                fos.write(numbers[i]);
+            fos.close();
+        } catch (IOException e) {
+            System.out.println("Error - " + e.toString());
+        }
+    }
+}
