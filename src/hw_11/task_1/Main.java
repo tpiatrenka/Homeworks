16d18666526e039609a3299a17ce34b621fea1ab
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 11:50:02 +0200

    Update Main.java

--- src/hw_11/task_1/Main.java
+++ src/hw_11/task_1/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//+
 //Вход на вечеринку только по списку.
 //Есть список имен (коллекция). Пользователь вводит с консоли свое имя, а программа проверяет его наличие в списке,
 //после чего говорит может он пройти или нет
@@ -18,13 +19,14 @@ public class Main {
         System.out.println("Введи имя");
         String scannerName = scanner.nextLine();
         boolean present = false;
+        //Можно использовать list.contains() 
         for (String name : nameList) {
             if (scannerName.equals(name)) {
                 present = true;
                 break;
             }
         }
-        String result = present ? "Можешь пройти" : "Не можешь пройти";
+        String result = present ? "Можешь пройти" : "Не можешь пройти"; //когда переменная объявляется и сразу один раз используется один раз, ее можно не заводить
         System.out.println(result);
     }
 }
