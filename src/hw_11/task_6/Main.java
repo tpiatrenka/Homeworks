01f2355801761020bc4660a1566b7fe042ab07b4
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:00:42 +0200

    Update Main.java

--- src/hw_11/task_6/Main.java
+++ src/hw_11/task_6/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+//+, но опять же лучше добавить переменную с количеством элементов 
 //Есть ArrayList и LinkedList на 100К одинаковых элементов.
 //Нужно написать бенчмарк, который бы показал скорость удаления из НАЧАЛА у двух этих листов
 //Удалить нужно все элементы
@@ -13,7 +14,8 @@ public class Main {
         for (int i = 0; i < 100_000; i++) {
             arrayNumbers.add(i);
         }
-
+        
+        //start -> beginning of the
         //delete from the start ArrayList
         long start = System.currentTimeMillis();
         for (int i = 0; i < 100_000; i++) {
@@ -28,7 +30,7 @@ public class Main {
         }
         start = System.currentTimeMillis();
         for (int i = 0; i < 100_000; i++) {
-            linkedListNumbers.remove(0);
+            linkedListNumbers.remove(0); //у Deque есть метод removeFirst()
         }
         end = System.currentTimeMillis();
         System.out.println("LinkedList time = " + (end - start));
