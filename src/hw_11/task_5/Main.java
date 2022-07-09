ada19f9e3592832852258c2e8dfabf4a17362b9c
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 11:56:35 +0200

    Update Main.java

--- src/hw_11/task_5/Main.java
+++ src/hw_11/task_5/Main.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+//+-
 //Есть ArrayList и LinkedList на 100К одинаковых элементов.
 //Нужно написать бенчмарк, который бы показал скорость удаления из СЕРЕДИНЫ у двух этих листов
 //Удалить нужно все элементы
 public class Main {
     public static void main(String[] args) {
         List<Integer> arrayNumbers = new ArrayList<>();
+        //Количество элементов лучше закинуть в переменную - при их изменени нужно будет поменять только в одном месте, сейчас - в 4ёх
         for (Integer i = 0; i < 100_000; i++) {
             arrayNumbers.add(i);
         }
@@ -27,6 +29,7 @@ public class Main {
             linkedListNumbers.add(i);
         }
         start = System.currentTimeMillis();
+        //А вот и проблемка с отсутствием переменной - количество итераций меньше, чем в других циклах
         for (Integer i = 0; i < 100_00; i++) {
             linkedListNumbers.remove(linkedListNumbers.size() / 2);
         }
