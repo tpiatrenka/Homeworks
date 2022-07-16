cfd79b9f234c76a77c7ad57a88e031218056d00b
Mary Stankevich <marrycitty@mail.ru>
2022-07-16 11:54:39 +0200

    Update Main.java

--- src/hw_12/task_3/Main.java
+++ src/hw_12/task_3/Main.java
@@ -2,6 +2,7 @@ package hw_12.task_3;
 
 import java.util.function.BinaryOperator;
 
+//+
 //Используя интерфейс BinaryOperator написать лямбда-выражение, которое бы умножало одно целое число на другое
 public class Main {
     public static void main(String[] args) {
