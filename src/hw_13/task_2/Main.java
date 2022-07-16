8e9a0fe0ea89e3a5cab025ffe1adbd31305592ee
Mary Stankevich <marrycitty@mail.ru>
2022-07-16 12:06:04 +0200

    Update Main.java

--- src/hw_13/task_2/Main.java
+++ src/hw_13/task_2/Main.java
@@ -4,10 +4,12 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+//+
 //Есть коллекция строк, нужно найти строку с максимальной длинной.
 public class Main {
     public static void main(String[] args) {
         List<String> stringList = List.of("Hello", "BYBYBY", "Weather", "JavaJavajava");
+        //название maxLength звучит скорее как число, так что лучше maxLengthString, например
         Optional<String> maxLength = stringList.stream().max(Comparator.comparing(String::length));
         System.out.println(maxLength);
     }
